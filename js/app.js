69364f7b317fd0e636fd0859de84fb0fe714d4d0
Michael Abood <Abood@Mikes-Macbook.local>
2017-07-19 18:42:35 -0500

    clean up

--- js/app.js
+++ js/app.js
@@ -1,20 +1,9 @@
-//start button for splash page
-// swap out images on the site - album art - avatar
-// make better questions
-//change font on text
-
-
-
-console.log("mike");
-
-
 function blink() {
    var blink = document.getElementById('start');
    setInterval(function() {
       blink.style.display = (blink.style.display == 'none' ? '' : 'none');
    }, 700);
 }
-
 
 var bothButtons = firstButton && secondButton; 
 
@@ -38,29 +27,31 @@ checkAnswer: function(action){
     this.level3()
   }
 
-    else if(action === "level4"){
+  else if(action === "level4"){
     this.level4()
   }
 
-      else if(action === "level5"){
+  else if(action === "level5"){
     this.level5()
   }
 
-      else if(action === "level6"){
+  else if(action === "level6"){
     this.level6()
   }
 
-        else if(action === "level7"){
+  else if(action === "level7"){
     this.level7()
   }
 
-          else if(action === "level8"){
+  else if(action === "level8"){
     this.level8()
   }
-       else if(action === "level1"){
+
+  else if(action === "level1"){
     this.level1()
 }
-    else if(action === "win"){
+
+  else if(action === "win"){
       this.win()
   }
 },
@@ -73,16 +64,13 @@ checkAnswer: function(action){
   },
 
   win: function(){
-      //need to delete button(s)    clearInterval(countdown)
     firstButton.innerHTML = "Do it again"
     secondButton.innerHTML = "NO"
     newQuestion.innerHTML = "Congratulations, You win absolutely nothing!"
     document.getElementById("button-2").style.visibility = "hidden";
     firstButton.dataset.action = "level1"
    
-
-
-    $('.modal-hidden').toggleClass('modal').html(0);
+  $('.modal-hidden').toggleClass('modal').html(0);
 },
 
     level1: function(){
@@ -93,10 +81,9 @@ checkAnswer: function(action){
     firstButton.dataset.action = "level2"
     secondButton.dataset.action = "level2"
     document.getElementById("avKanye").src = "images/avatars/tlop-av.jpg"
-     document.getElementById("albCover").src = "images/albums/tlop.png"
-     disLevel.innerHTML = "Current Level 1/8"
-
-  },
+    document.getElementById("albCover").src = "images/albums/tlop.png"
+    disLevel.innerHTML = "Current Level 1/8"
+},
 
   level2: function(){
     newQuestion.innerHTML = "Do you like fish sticks?"
@@ -108,8 +95,7 @@ checkAnswer: function(action){
     document.getElementById("avKanye").src = "images/avatars/yeezus-av.jpg"
      document.getElementById("albCover").src = "images/albums/yeezus.png"
      disLevel.innerHTML = "Current Level 2/8"
-
-  },
+},
 
   level3: function(){
     newQuestion.innerHTML = "Which artist do you chose to collaborate on an album with?"
@@ -117,11 +103,10 @@ checkAnswer: function(action){
     secondButton.innerHTML = "LUPE FIASCO"
     firstButton.dataset.action = "level4"
     secondButton.dataset.action = "die"
-     document.getElementById("avKanye").src = "images/avatars/throne-av.jpg"
-     document.getElementById("albCover").src = "images/albums/throne.png"
-     disLevel.innerHTML = "Current Level 3/8"
-
-   },
+    document.getElementById("avKanye").src = "images/avatars/throne-av.jpg"
+    document.getElementById("albCover").src = "images/albums/throne.png"
+    disLevel.innerHTML = "Current Level 3/8"
+},
 
 //   function endGame(){
 //   console.log('endGame is being called')
@@ -191,14 +176,10 @@ checkAnswer: function(action){
 }
 
 }; 
-
-
-function play(){
-       var audio = document.getElementById("audio");
-       audio.play();
-                 }
-
-                 
+// function play(){
+//        var audio = document.getElementById("audio");
+//        audio.play();
+//                  }; 
 
 var disLevel = document.getElementById("lvl")
 
@@ -217,13 +198,3 @@ secondButton.addEventListener('click' , function(e){
 });
 // var newButton1 = document.getElementById("button-1")
 var newQuestion = document.getElementsByClassName("question")[0]
-
-
-
-
-
-
-
-
-
-
